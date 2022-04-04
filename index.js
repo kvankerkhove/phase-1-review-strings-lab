669fbeaa7648faea072e3372a976beb3b3171539
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-04 12:52:59 -0400

    commit tree 55c48da89aa6c0a6ff1740ce2050221451d7e748

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Katey VanKerkhove';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
